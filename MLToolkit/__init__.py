5dd6c52505682a1192136681598c777f5846a25c
Sumudu Tennakoon <35412005+sptennak@users.noreply.github.com>
2019-06-23 00:08:13 -0400

    Add files via upload

---
+++ MLToolkit/__init__.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# MLToolkit (mltoolkit)
+
+__docformat__ = 'restructuredtext'
+__name__="MLToolkit"
+__version__="0.1.2"
+__author__="Sumudu Tennakoon"
+__create_date__="Sun Jul 01 2018"
+__last_update__="Sun Jun 22 2018"
+__license__="""
+Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+"""
+__doc__="""
+MLToolkit - a verstile helping library for machine learning
+===========================================================
+'MLToolkit' is a Python package providing a set of user-friendly functions to 
+help building machine learning models in data science research or production 
+focused projects. It is compatible with and interoperate with popular data 
+analysis, manipulation and machine learning libraries Pandas, Sci-kit Learn, 
+Tensorflow, Statmodels, Catboost, XGboost, etc.
+
+Main Features
+-------------
+- Data Extraction (SQL, Flatfiles, etc.)
+- Exploratory data analysis (statistical summary, univariate analysis, etc.)
+- Feature Extraction and Engineering
+- Model performance analysis and comparison between models
+- Hyper parameter tuning
+- Auto ML (automated machine learning)
+
+Links
+-----
+Website: http://sumudu.tennakoon.net/projects/MLToolkit
+Github: https://github.com/sptennak/MLToolkit
+"""
+
+hard_dependencies = ('numpy', 'scipy', 'matplotlib', 'pandas','sklearn', 'statsmodels','re') #'tensorflow'
+missing_dependencies = []
+
+for dependency in hard_dependencies:
+    try:
+        __import__(dependency)
+    except ImportError as e:
+        missing_dependencies.append(dependency)
+
+if missing_dependencies:
+    raise ImportError(
+        "Following packages are required but missing in the Python distribution: {}".format(missing_dependencies))
+del hard_dependencies, dependency, missing_dependencies
+
+print('MLToolkit=={}'.format(__version__.strip()))
+
+from MLToolkit.etl import *
+from MLToolkit.explore import *
+#from MLToolkit.model import *
+#from MLToolkit.deploy import *
